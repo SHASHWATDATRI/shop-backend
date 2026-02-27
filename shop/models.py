f36287a0d3618cd1abfafba2fc7d4985aff21bef
SHASHWAT DWIVEDI <shashwatdwivedi67@gmail.com>
2026-02-27 15:06:12 +0530

    Final URL routing and database fix

--- shop/models.py
+++ shop/models.py
@@ -3,13 +3,15 @@ from django.db import models
 # Create your models here.from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    # JPEG images automatic Cloudinary par jayengi
-    image = models.ImageField(upload_to='products/')
+    # Purana upload field
+    image = models.ImageField(upload_to='products/', null=True, blank=True) 
+    # Naya link wala field
+    image_url = models.URLField(max_length=500, null=True, blank=True) 
     stock = models.IntegerField(default=10)
-    created_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.name
