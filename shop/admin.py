b1619c28336964b8ce3fca4c5fb5b26058becd60
SHASHWAT DWIVEDI <shashwatdwivedi67@gmail.com>
2026-02-27 15:16:19 +0530

    Database structure updated with image_url

--- shop/admin.py
+++ shop/admin.py
@@ -3,4 +3,4 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'created_at')
+    list_display = ('name', 'price', 'stock')
