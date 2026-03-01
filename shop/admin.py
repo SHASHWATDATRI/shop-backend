746542596f08a7ff251630d54d9efc401ca9deaa
SHASHWAT DWIVEDI <shashwatdwivedi67@gmail.com>
2026-03-01 10:52:55 +0530

    Complete Artist and Product setup with fixed spellings

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Artist  # Dono ko import karna zaroori hai
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
+    list_display = ('id', 'name', 'price', 'stock')
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    # 'arlist_type' ko hata kar 'artist_type' (T ke saath) likhiye
+    list_display = ('id', 'name', 'designation', 'artist_type')
--- shop/models.py
+++ shop/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 
-# Create your models here.from django.db import models
-
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    # Purana upload field
-    image = models.ImageField(upload_to='products/', null=True, blank=True) 
-    # Naya link wala field
-    image_url = models.URLField(max_length=500, null=True, blank=True) 
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image_url = models.URLField(max_length=500, null=True, blank=True)
     stock = models.IntegerField(default=10)
-    
+
+    def __str__(self):
+        return self.name
+
+# Naya Artist Table
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+    artist_type = models.CharField(max_length=100)
+    image_url = models.URLField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.name
